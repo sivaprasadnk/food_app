fcd3fd9489acd068152d43031ac73ab97ee35db0
sivaprasadnk <sivaprasadnk123@gmail.com>
2024-08-22 17:52:39 +0530

    adding fab and appbar corrections

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/core/constant_colors.dart';
 import 'package:food_app/presentation/provider/filter_provider.dart';
 import 'package:food_app/presentation/widgets/filter_list.dart';
 import 'package:food_app/presentation/widgets/selected_filter_container.dart';
@@ -24,7 +25,11 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.arrow_back_ios_rounded),
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 15),
+          child: Icon(Icons.arrow_back_ios_rounded),
+        ),
+        leadingWidth: 35,
         title: const Text(
           'Filter Options',
           style: TextStyle(
@@ -32,6 +37,24 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      floatingActionButton: Container(
+        height: 50,
+        width: 150,
+        margin: const EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+          color: kBlackColor.withOpacity(0.7),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: const Center(
+          child: Text(
+            " SHOW 0 RESULTS",
+            style: TextStyle(
+              color: kWhiteColor,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: const Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
         child: Column(
