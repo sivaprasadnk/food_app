250882f0362ec2a5c00bd82d98418326821af204
sivaprasadnk <sivaprasadnk123@gmail.com>
2024-08-22 17:57:12 +0530

    adding comments

--- lib/presentation/provider/filter_provider.dart
+++ lib/presentation/provider/filter_provider.dart
@@ -7,37 +7,46 @@ import 'package:food_app/domain/entity/taxonomy.dart';
 import 'package:food_app/domain/use_cases/get_filters.dart';
 
 class FilterProvider extends ChangeNotifier {
+  /// to store list from backend
   List<Filter> _filterList = [];
   List<Filter> get filterList => _filterList;
 
+
+  /// to store selected filters
   List<Taxonomy> _selected = [];
   List<Taxonomy> get selected => _selected;
 
   FilterProvider(this.getFilters);
   GetFilters getFilters;
 
+
+  /// calling api
   Future getData() async {
     var resp = await locator<GetFilters>().call();
     _filterList = resp.$1!;
     notifyListeners();
   }
 
+  /// for selecting multiple select options
   addMutliSelect(Taxonomy item) {
     _selected.add(item);
     notifyListeners();
   }
 
+  /// for selecting single select options
   addSingleSelect(Taxonomy item) {
     _selected.removeWhere((e) => e.filterId == item.filterId);
     _selected.add(item);
     notifyListeners();
   }
 
+  /// removing selected filter on clicking the list above, or again tapping selected item
   removeFilter(Taxonomy item) {
     _selected.removeWhere((e) => e.slug == item.slug);
     notifyListeners();
   }
 
+  /// to display count of selected filters for a type
   int selectedCount(String id) {
     return selected.where((e) => e.filterId == id).length;
   }
